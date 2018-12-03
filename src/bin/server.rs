9f9d239017c12147a7cb6cef3a424326e48136c4
Piet Geursen <pietgeursen@gmail.com>
2018-12-04 11:01:45 +1300

    pseudocode startup

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -6,7 +6,25 @@ use jsonrpc_core::*;
 use jsonrpc_http_server::*;
 use flumedb::*; 
 
+
 fn main() {
+
+    //Pseudo code of server start up
+    //
+    // - file path.
+    // - load log with path, gets the latest seq value. 
+    // - load persisted views. 
+    // - rebuild views that need it  
+    //   - if they've just been created they need the whole log.
+    //   - or if view and log out of sync just rebuild as a first pass. 
+    // -  
+    // - move it all into a thread that polls channels 
+    //   - where do the views live?
+    //   - how do we satisfy the usecase where views are getting built and a client wants to query
+    //   a view? Most of the time one whole new view might be rebuilt, or maybe we're doing initial
+    //   sync, but in that case the bottleneck shouldn't be flume, it'll be crypto and calls to
+    //   query a view should still get serviced.
+    // - 
 
 
     let mut io = IoHandler::new();
