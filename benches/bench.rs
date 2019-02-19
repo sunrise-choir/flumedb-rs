b77f77b4a9862b2709efba607ffd4af9c11764ee
sean billig <sean.billig@gmail.com>
2019-02-18 20:09:36 -0600

    Clean up benchmarks; remove file creation from bench loop

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::*;
 
 extern crate serde;
 extern crate serde_json;
@@ -9,13 +9,25 @@ extern crate serde_json;
 extern crate byteorder;
 extern crate bytes;
 extern crate flumedb;
+extern crate tempfile;
 
 use flumedb::flume_log::FlumeLog;
 use flumedb::mem_log::MemLog;
 use flumedb::offset_log::*;
 use serde_json::{from_slice, Value};
+use tempfile::tempfile;
 
-const NUM_ENTRIES: u32 = 10000;
+const NUM_ENTRIES: usize = 10000;
+
+static DEFAULT_TEST_BUF: &[u8] = b"{\"value\": 1}";
+
+fn default_test_bufs() -> Vec<&'static [u8]> {
+    vec![DEFAULT_TEST_BUF; NUM_ENTRIES]
+}
+
+fn temp_offset_log() -> OffsetLog<u32> {
+    OffsetLog::<u32>::from_file(tempfile().unwrap()).unwrap()
+}
 
 fn offset_log_decode(c: &mut Criterion) {
     c.bench_function("offset_log_decode", |b| {
@@ -27,117 +39,123 @@ fn offset_log_decode(c: &mut Criterion) {
         })
     });
 }
+
 fn offset_log_append(c: &mut Criterion) {
+    let buf = DEFAULT_TEST_BUF;
     c.bench_function("offset log append", move |b| {
-        b.iter(|| {
-            let filename = "/tmp/test123.offset".to_string(); //careful not to reuse this filename, threads might make things weird
-            std::fs::remove_file(filename.clone()).unwrap_or(());
-
-            let test_vec = b"{\"value\": 1}";
-
-            let mut offset_log = OffsetLog::<u32>::new(filename).unwrap();
-
-            let offsets: Vec<u64> = (0..NUM_ENTRIES)
-                .map(|_| offset_log.append(test_vec).unwrap())
-                .collect();
-
-            assert_eq!(offsets.len(), NUM_ENTRIES as usize);
-        })
+        b.iter_batched(temp_offset_log,
+                     |mut log| {
+                         for i in 0..NUM_ENTRIES {
+                             let offset = log.append(buf).unwrap() as usize;
+                             assert_eq!(offset, i * (12 + buf.len()));
+                         }
+                     },
+                     BatchSize::SmallInput);
     });
 }
 
 fn offset_log_append_batch(c: &mut Criterion) {
-    let test_vec: &[u8] = b"{\"value\": 1}";
 
-    let mut test_vecs = Vec::new();
-
-    for _ in 0..NUM_ENTRIES {
-        test_vecs.push(test_vec);
-    }
-
-    c.bench_function("offset log append batch", move |b| {
-        b.iter(|| {
-            let filename = "/tmp/test123.offset".to_string(); //careful not to reuse this filename, threads might make things weird
-            std::fs::remove_file(filename.clone()).unwrap_or(());
-
-            let mut offset_log = OffsetLog::<u32>::new(filename).unwrap();
-
-            let result = offset_log.append_batch(&test_vecs).unwrap();
-
-            assert_eq!(result[0], 0);
-        })
+    let test_bufs = default_test_bufs();
+    c.bench_function("offset log append batch - all", move |b| {
+        b.iter_batched(temp_offset_log,
+                       |mut log| {
+                           let offsets = log.append_batch(&test_bufs).unwrap();
+                           assert_eq!(offsets.len(), NUM_ENTRIES);
+                           assert_eq!(offsets[0], 0);
+                       },
+                       BatchSize::SmallInput);
     });
+
+    let test_bufs = default_test_bufs();
+    c.bench_function("offset log append batch - 100", move |b| {
+        b.iter_batched(temp_offset_log,
+                       |mut log| {
+                           for chunk in test_bufs.chunks(100) {
+                               let offsets = log.append_batch(&chunk).unwrap();
+                               assert_eq!(offsets.len(), chunk.len());
+                           }
+                       },
+                       BatchSize::SmallInput);
+    });
+
 }
 
 fn offset_log_get(c: &mut Criterion) {
-    let filename = "/tmp/test123.offset".to_string(); //careful not to reuse this filename, threads might make things weird
-    std::fs::remove_file(filename.clone()).unwrap_or(());
-
-    let test_vec = b"{\"value\": 1}";
-
-    let mut offset_log = OffsetLog::<u32>::new(filename).unwrap();
-
-    let offsets: Vec<u64> = (0..NUM_ENTRIES)
-        .map(|_| offset_log.append(test_vec).unwrap())
-        .collect();
+    let mut log = temp_offset_log();
+    let offsets = log.append_batch(&default_test_bufs()).unwrap();
 
     c.bench_function("offset log get", move |b| {
         b.iter(|| {
-            for offset in offsets.clone() {
-                let result = offset_log.get(offset).unwrap();
-                assert_eq!(result.len(), test_vec.len());
+            for offset in offsets.iter() {
+                let result = log.get(*offset).unwrap();
+                assert_eq!(result.len(), DEFAULT_TEST_BUF.len());
             }
         })
     });
 }
 
 fn offset_log_iter(c: &mut Criterion) {
-    let filename = "/tmp/test123.offset".to_string(); //careful not to reuse this filename, threads might make things weird
-    std::fs::remove_file(filename.clone()).unwrap_or(());
 
-    let test_vec = b"{\"value\": 1}";
+    // Forward
+    let mut log = temp_offset_log();
+    let offsets = log.append_batch(&default_test_bufs()).unwrap();
 
-    let mut offset_log = OffsetLog::<u32>::new(filename.clone()).unwrap();
+    c.bench_function("offset log iter forward", move |b| {
+        b.iter_batched(|| log.iter(),
+                       |mut iter| {
+                           let count = iter.forward().count();
+                           assert_eq!(count, offsets.len());
+                       },
+                       BatchSize::SmallInput)});
 
-    let offsets: Vec<u64> = (0..NUM_ENTRIES)
-        .map(|_| offset_log.append(test_vec).unwrap())
-        .collect();
+    // Backward
+    let mut log = temp_offset_log();
+    let offsets = log.append_batch(&default_test_bufs()).unwrap();
 
-    assert_eq!(offsets.len(), NUM_ENTRIES as usize);
+    c.bench_function("offset log iter backward", move |b| {
+        b.iter_batched(|| log.iter_at_offset(log.end()),
+                       |mut iter| {
+                           let count = iter.backward().count();
+                           assert_eq!(count, offsets.len());
+                       },
+                       BatchSize::SmallInput)});
 
-    c.bench_function("offset log iter", move |b| {
-        b.iter(|| {
-            let sum: u64 = offset_log.iter().forward()
-                .map(|val| val.data)
-                .map(|val| from_slice(&val).unwrap())
-                .map(|val: Value| match val["value"] {
-                    Value::Number(ref num) => {
-                        let result = num.as_u64().unwrap();
-                        result
-                    }
-                    _ => panic!(),
-                })
-                .sum();
+    // Forward with json decoding
+    let mut log = temp_offset_log();
+    log.append_batch(&default_test_bufs()).unwrap();
 
-            assert!(sum > 0);
-        })
-    });
+    c.bench_function("offset log iter forward and json decode", move |b| {
+        b.iter_batched(|| log.iter(),
+                       |mut iter| {
+                           let sum: u64 = iter.forward()
+                               .map(|val| from_slice(&val.data).unwrap())
+                               .map(|val: Value| match val["value"] {
+                                   Value::Number(ref num) => {
+                                       let r: u64 = num.as_u64().unwrap();
+                                       r
+                                   }
+                                   _ => panic!(),
+                               })
+                               .sum();
+                           assert_eq!(sum, NUM_ENTRIES as u64);
+                       },
+                       BatchSize::SmallInput)});
+
 }
 
 fn mem_log_get(c: &mut Criterion) {
     let mut log = MemLog::new();
-
-    let test_vec = b"{\"value\": 1}";
-
+    let test_buf = DEFAULT_TEST_BUF;
     let offsets: Vec<u64> = (0..NUM_ENTRIES)
-        .map(|_| log.append(test_vec).unwrap())
+        .map(|_| log.append(test_buf).unwrap())
         .collect();
 
     c.bench_function("mem log get", move |b| {
         b.iter(|| {
-            for offset in offsets.clone() {
-                let result = log.get(offset).unwrap();
-                assert_eq!(result.len(), test_vec.len());
+            for offset in offsets.iter() {
+                let result = log.get(*offset).unwrap();
+                assert_eq!(result.len(), test_buf.len());
             }
         })
     });
@@ -148,10 +166,8 @@ fn mem_log_append(c: &mut Criterion) {
         b.iter(|| {
             let mut log = MemLog::new();
 
-            let test_vec = b"{\"value\": 1}";
-
             let offsets: Vec<u64> = (0..NUM_ENTRIES)
-                .map(|_| log.append(test_vec).unwrap())
+                .map(|_| log.append(DEFAULT_TEST_BUF).unwrap())
                 .collect();
 
             assert_eq!(offsets.len(), NUM_ENTRIES as usize);
@@ -162,13 +178,11 @@ fn mem_log_append(c: &mut Criterion) {
 fn mem_log_iter(c: &mut Criterion) {
     let mut log = MemLog::new();
 
-    let test_vec = b"{\"value\": 1}";
-
     (0..NUM_ENTRIES).for_each(|_| {
-        log.append(test_vec).unwrap();
+        log.append(DEFAULT_TEST_BUF).unwrap();
     });
 
-    c.bench_function("mem log iter", move |b| {
+    c.bench_function("mem log iter and json decode", move |b| {
         b.iter(|| {
             let sum: u64 = log
                 .into_iter()
